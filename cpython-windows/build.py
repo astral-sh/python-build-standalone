527957b89aed0cefbe3e7e8287643d087764bc5a
Zanie Blue <contact@zanie.dev>
2025-04-16 08:49:45 -0500

    Update abi platform

--- cpython-windows/build.py
+++ cpython-windows/build.py
@@ -1106,7 +1106,7 @@ def collect_python_build_artifacts(
     elif arch == "win32":
         abi_platform = "win32"
     elif arch == "arm64":
-        abi_platform = "arm64"
+        abi_platform = "win__arm64"
     else:
         raise ValueError("unhandled arch: %s" % arch)
 

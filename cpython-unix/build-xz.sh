0ebefadaa0ce203a7545045ed8bca860c1f966a3
Jonathan Helmus <jjhelmus@gmail.com>
2025-11-14 16:24:08 -0600

    use EXTRA_CONFIGURE_FLAGS in build-xz.sh

--- cpython-unix/build-xz.sh
+++ cpython-unix/build-xz.sh
@@ -13,16 +13,16 @@ tar -xf xz-${XZ_VERSION}.tar.gz
 
 pushd xz-${XZ_VERSION}
 
-skip_werror_check=no
+EXTRA_CONFIGURE_FLAGS=
 
 # musl-clang injects flags that are not used during compilation,
 # e.g. -fuse-ld=musl-clang. These raise warnings that can be ignored but
 # cause the -Werror check to fail. Skip the check.
 if [ "${CC}" = "musl-clang" ]; then
-    skip_werror_check=yes
+    EXTRA_CONFIGURE_FLAGS="${EXTRA_CONFIGURE_FLAGS} SKIP_WERROR_CHECK=yes"
 fi
 
-SKIP_WERROR_CHECK="${skip_werror_check}" CFLAGS="${EXTRA_TARGET_CFLAGS} -fPIC" CPPFLAGS="${EXTRA_TARGET_CFLAGS} -fPIC" CCASFLAGS="${EXTRA_TARGET_CFLAGS} -fPIC" LDFLAGS="${EXTRA_TARGET_LDFLAGS}" ./configure \
+CFLAGS="${EXTRA_TARGET_CFLAGS} -fPIC" CPPFLAGS="${EXTRA_TARGET_CFLAGS} -fPIC" CCASFLAGS="${EXTRA_TARGET_CFLAGS} -fPIC" LDFLAGS="${EXTRA_TARGET_LDFLAGS}" ./configure \
     --build=${BUILD_TRIPLE} \
     --host=${TARGET_TRIPLE} \
     --prefix=/tools/deps \
@@ -32,7 +32,8 @@ SKIP_WERROR_CHECK="${skip_werror_check}" CFLAGS="${EXTRA_TARGET_CFLAGS} -fPIC" C
     --disable-lzmadec \
     --disable-lzmainfo \
     --disable-lzma-links \
-    --disable-scripts
+    --disable-scripts \
+    ${EXTRA_CONFIGURE_FLAGS}
 
 make -j ${NUM_CPUS}
 make -j ${NUM_CPUS} install DESTDIR=${ROOT}/out

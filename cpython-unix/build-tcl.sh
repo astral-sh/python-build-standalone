40fa1577b69e07173777ebf59911261582ec44d4
Jonathan Helmus <jjhelmus@gmail.com>
2025-12-19 09:06:41 -0600

    disable fts64_ functions on armv7

--- cpython-unix/build-tcl.sh
+++ cpython-unix/build-tcl.sh
@@ -64,6 +64,12 @@ if [ -n "${CROSS_COMPILING}" ]; then
     EXTRA_CONFIGURE="${EXTRA_CONFIGURE} --disable-zipfs"
 fi
 
+# Disable the use of fts64_* functions on the 32-bit armv7 platform as these
+# functions are not available in glibc 2.17
+if [[ ${TARGET_TRIPLE} = armv7* ]]; then
+    EXTRA_CONFIGURE="${EXTRA_CONFIGURE} tcl_cv_flag__file_offset_bits=no"
+fi
+
 # musl does not include queue.h
 # https://wiki.musl-libc.org/faq#Q:-Why-is-%3Ccode%3Esys/queue.h%3C/code%3E-not-included?
 # It is a self contained header file, use a copy from the container.

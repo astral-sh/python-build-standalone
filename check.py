61c91fb98cb824de41fd6ddd3cd205afefbf94b7
Zsolt Dollenstein <zsol.zsol@gmail.com>
2026-03-25 11:30:59 +0000

    CI: use uv instead of pip

--- check.py
+++ check.py
@@ -10,42 +10,24 @@ import os
 import pathlib
 import subprocess
 import sys
-import venv
 
 ROOT = pathlib.Path(os.path.abspath(__file__)).parent
-VENV = ROOT / "venv.dev"
-PIP = VENV / "bin" / "pip"
-PYTHON = VENV / "bin" / "python"
-REQUIREMENTS = ROOT / "requirements.dev.txt"
-
-
-def bootstrap():
-    venv.create(VENV, with_pip=True)
-
-    subprocess.run([str(PIP), "install", "-r", str(REQUIREMENTS)], check=True)
-
-    os.environ["PYBUILD_BOOTSTRAPPED"] = "1"
-    os.environ["PATH"] = "%s:%s" % (str(VENV / "bin"), os.environ["PATH"])
-    os.environ["PYTHONPATH"] = str(ROOT)
-
-    args = [str(PYTHON), __file__, *sys.argv[1:]]
-
-    os.execv(str(PYTHON), args)
 
 
 def run_command(command: list[str]) -> int:
-    print("$ " + " ".join(command))
+    print("$ " + " ".join(command), flush=True)
     returncode = subprocess.run(
-        command, stdout=sys.stdout, stderr=sys.stderr
+        command,
+        cwd=ROOT,
+        stdout=sys.stdout,
+        stderr=sys.stderr,
+        env={**os.environ, "PYTHONUNBUFFERED": "1"},
     ).returncode
     print()
     return returncode
 
 
-def run():
-    env = dict(os.environ)
-    env["PYTHONUNBUFFERED"] = "1"
-
+def run() -> None:
     parser = argparse.ArgumentParser(description="Check code.")
     parser.add_argument(
         "--fix",
@@ -54,31 +36,19 @@ def run():
     )
     args = parser.parse_args()
 
-    check_args = []
-    format_args = []
-    mypy_args = []
+    check_args = ["--fix"] if args.fix else []
+    format_args = [] if args.fix else ["--check"]
 
-    if args.fix:
-        check_args.append("--fix")
-    else:
-        format_args.append("--check")
-
-    check_result = run_command(["ruff", "check"] + check_args)
-    format_result = run_command(["ruff", "format"] + format_args)
-    mypy_result = run_command(["mypy"] + mypy_args)
+    check_result = run_command(["uv", "run", "ruff", "check", *check_args])
+    format_result = run_command(["uv", "run", "ruff", "format", *format_args])
+    mypy_result = run_command(["uv", "run", "mypy"])
 
     if check_result + format_result + mypy_result:
         print("Checks failed!")
         sys.exit(1)
-    else:
-        print("Checks passed!")
+
+    print("Checks passed!")
 
 
 if __name__ == "__main__":
-    try:
-        if "PYBUILD_BOOTSTRAPPED" not in os.environ:
-            bootstrap()
-        else:
-            run()
-    except subprocess.CalledProcessError as e:
-        sys.exit(e.returncode)
+    run()

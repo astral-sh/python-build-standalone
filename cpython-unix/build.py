7008378dd2c558839b1a6a8d98188afce98887df
Jonathan Helmus <jjhelmus@gmail.com>
2026-04-21 15:04:10 -0400

    binutils not required when cross-compiling riscv

--- cpython-unix/build.py
+++ cpython-unix/build.py
@@ -249,8 +249,10 @@ def toolchain_archive_path(package_name, host_platform):
     return BUILD / basename
 
 
-def install_binutils(platform):
-    return not platform.startswith("macos_")
+def install_binutils(host_platform, target_triple):
+    if "riscv64" in target_triple:
+        return False
+    return not host_platform.startswith("macos_")
 
 
 def simple_build(
@@ -275,7 +277,7 @@ def simple_build(
                 host_platform,
                 target_triple,
                 binutils_image=docker_image_names(settings)["gcc"],
-                binutils=install_binutils(host_platform),
+                binutils=install_binutils(host_platform, target_triple),
                 clang=True,
                 musl="musl" in target_triple,
                 static="static" in build_options,
@@ -399,7 +401,7 @@ def build_libedit(
                 host_platform,
                 target_triple,
                 binutils_image=docker_image_names(settings)["gcc"],
-                binutils=install_binutils(host_platform),
+                binutils=install_binutils(host_platform, target_triple),
                 clang=True,
                 musl="musl" in target_triple,
                 static="static" in build_options,
@@ -452,7 +454,7 @@ def build_cpython_host(
             host_platform,
             target_triple,
             binutils_image=docker_image_names(settings)["gcc"],
-            binutils=install_binutils(host_platform),
+            binutils=install_binutils(host_platform, target_triple),
             clang=True,
             static="static" in build_options,
         )
@@ -791,7 +793,7 @@ def build_cpython(
                 host_platform,
                 target_triple,
                 binutils_image=docker_image_names(settings)["gcc"],
-                binutils=install_binutils(host_platform),
+                binutils=install_binutils(host_platform, target_triple),
                 clang=True,
                 musl="musl" in target_triple,
                 static="static" in build_options,

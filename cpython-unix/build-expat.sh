096b39e6e7e8c82b274427ad237cbdc19243e38b
Jonathan J. Helmus <jjhelmus@gmail.com>
2026-07-21 09:48:15 -0500

    opt-in to /dev/urandom for expat on x86-64

--- cpython-unix/build-expat.sh
+++ cpython-unix/build-expat.sh
@@ -13,6 +13,15 @@ tar -xf "expat-${EXPAT_VERSION}.tar.xz"
 
 pushd "expat-${EXPAT_VERSION}"
 
+EXPAT_CONFIGURE_FLAGS=()
+# Expat 2.8.2 no longer enables /dev/urandom by default. Enable it for
+# # x86-64 Linux, whose older glibc target cannot use getrandom().
+# https://github.com/libexpat/libexpat/pull/1257
+if [[ "${TARGET_TRIPLE}" = x86_64-*-linux-* ]]; then
+    # Older Linux targets may not expose newer entropy APIs to configure.
+    EXPAT_CONFIGURE_FLAGS+=(--with-dev-urandom)
+fi
+
 # xmlwf isn't needed by CPython.
 # Disable -fexceptions because we don't need it and it adds a dependency on libgcc_s, which
 # is softly undesirable.
@@ -24,6 +33,7 @@ CFLAGS="${EXTRA_TARGET_CFLAGS} -fPIC" CPPFLAGS="${EXTRA_TARGET_CFLAGS} -fPIC" ./
     --without-examples \
     --without-tests \
     --without-xmlwf \
+    "${EXPAT_CONFIGURE_FLAGS[@]}" \
     ax_cv_check_cflags___fexceptions=no
 
 make -j "${NUM_CPUS}"

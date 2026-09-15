3163a5023f010cd9cc34f896892858a6175cc269
Jonathan J. Helmus <jjhelmus@gmail.com>
2026-09-15 14:25:29 -0500

    find single VS install using -latest

--- cpython-windows/build.py
+++ cpython-windows/build.py
@@ -260,7 +260,7 @@ def find_vs_path(path, msvc_version):
         [
             str(vswhere),
             "-utf8",
-            # Visual Studio 2019.
+            "-latest",
             "-version",
             version,
             "-property",
